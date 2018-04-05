b11cc97dfcf63f8300159085ff8ae06bf0696422
kuron334 <kuron.yama@gmail.com>
2018-04-06 02:59:07 +0900

    2018-4-6

--- script/script.js
+++ script/script.js
@@ -1,6 +1,91 @@
-var vm = new Vue({
+var topMenuButton = new Vue({
 	el: '#menu-button',
 	data: {
 		toggle: true,
 	}
 })
+
+var drowerMenuChildItems = new Vue({
+	el: '#drower-menu-list',
+	data: {
+		lists: [
+			{
+				items: '最新情報',
+				childLists:[]
+			},
+			{
+				items: 'ヒーロー',
+				childLists: [
+					{ childItems: 'タンク' },
+					{ childItems: 'ファイター' },
+					{ childItems: 'アサシン' },
+					{ childItems: 'メイジ' },
+					{ childItems: 'マークスマン' },
+					{ childItems: 'サポート' }
+				]
+			},
+			{
+				items: 'おすすめ装備',
+				childLists: [
+					{ childItems: 'タンク' },
+					{ childItems: 'ファイター' },
+					{ childItems: 'アサシン' },
+					{ childItems: 'メイジ' },
+					{ childItems: 'マークスマン' },
+					{ childItems: 'サポート' }
+				]	
+			},
+			{
+				items: '遊び方',
+				childLists: [
+					{ childItems: '常識編' },
+					{ childItems: '基本編' },
+					{ childItems: '応用編' }
+				]
+			},
+			{
+				items: 'アイテム・スキル',
+				childLists: [
+					{ childItems: '各種アイテム' },
+					{ childItems: '汎用スキル' }
+				]	
+			},
+			{
+				items: '歴代環境',
+				childLists: [
+					{ childItems: '第4回バランス調整' },
+					{ childItems: '第3回バランス調整' },
+					{ childItems: '第2回バランス調整' },
+					{ childItems: '第1回バランス調整' },
+					{ childItems: 'リリース開始' }
+				]
+			}
+
+		]
+	}
+})
+
+
+/*
+var drowerMenuChildItems = new Vue({
+	el: '.drower-menu-child-list',
+	data: {
+		items1: [
+			{ childItems: 'タンク' },
+			{ childItems: 'ファイター' },
+			{ childItems: 'アサシン' },
+			{ childItems: 'メイジ' },
+			{ childItems: 'マークスマン' },
+			{ childItems: 'サポート' }
+		],
+		items2: [
+			{ childItems: 'タンク' },
+			{ childItems: 'ファイター' },
+			{ childItems: 'アサシン' },
+			{ childItems: 'メイジ' },
+			{ childItems: 'マークスマン' },
+			{ childItems: 'サポート' }
+		]
+	}
+})
+*/
